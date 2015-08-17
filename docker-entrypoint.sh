2a2ea43511d7cd352b84d69f49dd86edcfababf2
altuhov <altuhov@clickberry.com>
2015-08-17 11:59:23 +0300

    Merge branch 'master' of https://github.com/clickberry/auth-api-nodejs
    Conflicts:
    	config/development.json

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 
 # set env variables
 if [ -n "$MONGO_PORT_27017_TCP_ADDR" ] && [ -n "$MONGO_PORT_27017_TCP_PORT" ]; then
-  export mongo_connection="mongodb://${MONGO_PORT_27017_TCP_ADDR}:${MONGO_PORT_27017_TCP_PORT}/auth"
+  export MONGODB_CONNECTION="mongodb://${MONGO_PORT_27017_TCP_ADDR}:${MONGO_PORT_27017_TCP_PORT}/auth"
 fi
 
-echo "USING MONGO CONNECTION: ${mongo_connection}"
+echo "USING MONGO CONNECTION: ${MONGODB_CONNECTION}"
 
 # execute nodejs application
 exec /nodejs/bin/npm start
